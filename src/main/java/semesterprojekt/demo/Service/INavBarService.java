071c4fe620a46912fc73bdb28d11450660ba5d48
MathiasFH <Maddibro@live.dk>
2019-05-09 16:05:49 +0200

    First commit to new Branch

--- src/main/java/semesterprojekt/demo/Service/INavBarService.java
+++ src/main/java/semesterprojekt/demo/Service/INavBarService.java
@@ -2,13 +2,19 @@ package semesterprojekt.demo.Service;
 
 import org.springframework.stereotype.Service;
 import semesterprojekt.demo.Model.NavigationBar;
+import semesterprojekt.demo.Model.NavigationObjects;
+import semesterprojekt.demo.Model.NewsModel;
 
 import java.util.Optional;
 
 @Service
 public interface INavBarService {
 
+    NavigationObjects createNavObject(NavigationObjects navigationObjects);
+
     Optional<NavigationBar> findNavBarById(Long id);
+
+    void deleteNavObject(Long id);
 
 
 }
--- src/main/java/semesterprojekt/demo/Service/INavBarServiceImpl.java
+++ src/main/java/semesterprojekt/demo/Service/INavBarServiceImpl.java
@@ -3,18 +3,31 @@ package semesterprojekt.demo.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import semesterprojekt.demo.Model.NavigationBar;
+import semesterprojekt.demo.Model.NavigationObjects;
 import semesterprojekt.demo.Repo.NavBarRepo;
 
 import java.util.Optional;
 
 @Service
-public class INavBarServiceImpl implements INavBarService {
+public class INavBarServiceImpl implements INavBarService
+{
 
     @Autowired
     NavBarRepo navBarRepo;
 
     @Override
-    public Optional<NavigationBar> findNavBarById(Long id) {
+    public NavigationObjects createNavObject(NavigationObjects navigationObjects) {
+        return null;
+    }
+
+    @Override
+    public void deleteNavObject(Long id) {
+
+    }
+
+    @Override
+    public Optional<NavigationBar> findNavBarById(Long id)
+    {
         return navBarRepo.findById(id);
     }
 }
