36a3f6ba6b7beb5582fe42c49591f8570a67d2c4
Liam Blake <liamblakedk@hotmail.com>
2019-05-14 11:20:42 +0200

    refactoring done LOMBOK

--- src/main/java/semesterprojekt/demo/Model/Contact.java
+++ src/main/java/semesterprojekt/demo/Model/Contact.java
@@ -1,13 +1,22 @@
 package semesterprojekt.demo.Model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Contact {
-
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Contact
+{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,82 +34,5 @@ public class Contact {
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.adresse = adresse;
-    }
-
-    public Contact()
-    {
-    }
-
-    public Long getId()
-    {
-        return id;
-    }
-
-    public void setId(Long id)
-    {
-        this.id = id;
-    }
-
-    public String getFirstName()
-    {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName)
-    {
-        this.firstName = firstName;
-    }
-
-    public String getLastName()
-    {
-        return lastName;
-    }
-
-    public void setLastName(String lastName)
-    {
-        this.lastName = lastName;
-    }
-
-    public int getPhoneNumber()
-    {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(int phoneNumber)
-    {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail()
-    {
-        return email;
-    }
-
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
-
-    public String getAdresse()
-    {
-        return adresse;
-    }
-
-    public void setAdresse(String adresse)
-    {
-        this.adresse = adresse;
-    }
-
-    @Override
-    public String toString()
-    {
-        return "Contact{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", phoneNumber=" + phoneNumber +
-                ", email='" + email + '\'' +
-                ", adresse='" + adresse + '\'' +
-                '}';
     }
 }
